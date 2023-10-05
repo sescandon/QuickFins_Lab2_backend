173db398a4a12422137546fd40e0cf5526eaf367
XdmriveroeX <Dmriveroe@gmail.com>
2023-10-04 23:19:23 -0500

    associaciones

--- src/main/java/co/edu/unal/software_engineering/labs/controller/CourseController.java
+++ src/main/java/co/edu/unal/software_engineering/labs/controller/CourseController.java
@@ -8,14 +8,18 @@ import co.edu.unal.software_engineering.labs.pojo.CoursePOJO;
 import co.edu.unal.software_engineering.labs.pojo.EnrolledCoursePOJO;
 import co.edu.unal.software_engineering.labs.service.AssociationService;
 import co.edu.unal.software_engineering.labs.service.CourseService;
+import co.edu.unal.software_engineering.labs.service.PeriodService;
 import co.edu.unal.software_engineering.labs.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import co.edu.unal.software_engineering.labs.model.*;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,13 +29,17 @@ import java.util.List;
 public class CourseController{
 
     private final CourseService courseService;
-    private final AssociationService associationService;
+    private AssociationService associationService;
     private final UserService userService;
+    private PeriodService periodService;
 
-    public CourseController(CourseService courseService, AssociationService associationService, UserService userService) {
+
+    public CourseController(CourseService courseService, AssociationService associationService, UserService userService,PeriodService periodService) {
         this.courseService = courseService;
         this.associationService = associationService;
         this.userService = userService;
+        this.periodService = periodService;
+
     }
 
     @PostMapping(value = {"/profesor/crear-curso"})
@@ -39,19 +47,21 @@ public class CourseController{
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
         User professor = userService.findByUsername(username);
         Course course = courseService.mapperCourseEntity(coursePojo);
+        Period period = periodService.findById( 1 );
         Role role = new Role();  
-
-        if (professor == null || !courseService.isRightCourse(course)) {
+    
+        if (professor == null || period == null || !courseService.isRightCourse(course)) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
-
+    
         role.setId(Role.ROLE_TEACHER); 
         courseService.save(course);
-
-        associationService.associate(professor, role, course, null);
-
+    
+        associationService.associate(professor, role, course, period);
+    
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
+    
 
     @GetMapping("/mis-cursos")
     public List<EnrolledCoursePOJO> getCoursesByUser() {
