2376c17ae374144d75610d0ce418a6bfed0618ac
sescandon <sescandon@unal.edu.co>
2023-10-04 21:31:54 -0500

    Added teacher course enrollment

--- src/main/java/co/edu/unal/software_engineering/labs/controller/TeacherController.java
+++ src/main/java/co/edu/unal/software_engineering/labs/controller/TeacherController.java
@@ -1,10 +1,49 @@
 package co.edu.unal.software_engineering.labs.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import co.edu.unal.software_engineering.labs.model.*;
+import co.edu.unal.software_engineering.labs.service.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TeacherController{
 
+    private AssociationService associationService;
 
+    private UserService userService;
+
+    private CourseService courseService;
+
+    private PeriodService periodService;
+
+
+    public TeacherController( UserService userService, CourseService courseService, PeriodService periodService,
+                              AssociationService associationService ){
+        this.userService = userService;
+        this.courseService = courseService;
+        this.periodService = periodService;
+        this.associationService = associationService;
+    }
+
+
+    @PostMapping( value = { "/profesor/inscribir/periodo/{periodId}/curso/{courseId}" } )
+    public ResponseEntity<Void> associateTeacher( @PathVariable Integer periodId, @PathVariable Integer courseId ){
+        String username = SecurityContextHolder.getContext( ).getAuthentication( ).getName( );
+        User teacher = userService.findByUsername( username );
+        Course course = courseService.findById( courseId );
+        Period period = periodService.findById( periodId );
+        Role role = new Role( );
+
+        if( course == null || period == null ){
+            return new ResponseEntity<>( HttpStatus.BAD_REQUEST );
+        }
+
+        role.setId( Role.ROLE_TEACHER );
+        associationService.associate( teacher, role, course, period );
+
+        return new ResponseEntity<>( HttpStatus.CREATED );
+    }
 
 }
